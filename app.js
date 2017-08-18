f7ed0270cc198e1188073cd36b8f38343c385a82
opatel99 <ohmjpatel99@gmail.com>
2017-08-18 00:11:35 -0500

    simple server boilerplate to start

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var mongoose = require('mongoose');
+var nunjucks = require('nunjucks');
+
+var app = express();
+var port = process.env.PORT || 8080;
+app.listen(port)
+console.log(`app listening on port: ${port}`);
+
+nunjucks.configure('views', {
+	autoescape: true,
+	express: app
+});
+
+app.get('/', function (req, res) {
+	res.render('index.html');
+});
+
